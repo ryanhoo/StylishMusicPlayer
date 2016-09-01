4cb24b80dfba475e5588334ab652abeb83197d56
ryanhoo <ryan.hoo.j@gmail.com>
2016-09-01 22:05:32 +0800

    feat(Main): Toolbar controls

---
+++ app/src/main/java/io/github/ryanhoo/music/ui/playlist/PlayListFragment.java
@@ -0,0 +1,25 @@
+package io.github.ryanhoo.music.ui.playlist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import io.github.ryanhoo.music.R;
+import io.github.ryanhoo.music.ui.base.BaseFragment;
+
+/**
+ * Created with Android Studio.
+ * User: ryan.hoo.j@gmail.com
+ * Date: 9/1/16
+ * Time: 9:58 PM
+ * Desc: PlayListFragment
+ */
+public class PlayListFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_play_list, container, false);
+    }
+}
