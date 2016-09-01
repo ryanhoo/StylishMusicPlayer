348e443266f73b4dc5a5db70b7ba817252d248b6
ryanhoo <ryan.hoo.j@gmail.com>
2016-09-02 01:36:59 +0800

    feat(SegmentedControl): Implement segmented control by customizing radio button

--- app/src/main/java/io/github/ryanhoo/music/ui/local/LocalFilesFragment.java
+++ app/src/main/java/io/github/ryanhoo/music/ui/local/LocalFilesFragment.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.ryanhoo.music.R;
 import io.github.ryanhoo.music.ui.base.BaseFragment;
 
@@ -17,9 +20,22 @@ import io.github.ryanhoo.music.ui.base.BaseFragment;
  */
 public class LocalFilesFragment extends BaseFragment {
 
+    @BindView(R.id.radio_button_all)
+    RadioButton radioButtonAll;
+    @BindView(R.id.radio_button_folder)
+    RadioButton radioButtonFolder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_local_files, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        radioButtonAll.setChecked(true);
     }
 }
